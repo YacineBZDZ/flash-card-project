cd781058ea5b6b2cb8bae721e140b8d62a31c9a6
yacine <jakmca18@gmail.com>
2024-04-25 00:19:58 +0200

    Implementation of the right and wrong button

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter import *
 BACKGROUND_COLOR = "#B1DDC6"
 FONT = "Ariel"
-window = Tk()
 window.minsize(width=1000, height=600)
 window.title("Flashy")
 window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
@@ -14,13 +13,11 @@ card_front.create_text(400,263, text="word",font=(FONT,60,"bold"))
 card_front.grid(column=1,row=1,columnspan=2)
 
 image_right_but = PhotoImage(file="../flash-card-project-start/images/right.png")
-right_but = Canvas(width=100,height=100,highlightthickness=0)
-right_but.create_image(50,50, image=image_right_but)
+right_but = Button(width=100,height=100,image=image_right_but, bd=0,bg=BACKGROUND_COLOR, highlightthickness=0)
 right_but.grid(column=2,row=2)
 
-image_wrong_but = PhotoImage(file="../flash-card-project-start/images/wrong.png")
-wrong_but = Canvas(width=100, height=100,highlightthickness=0)
-wrong_but.create_image(50,50, image=image_wrong_but)
+image_wrong = PhotoImage(file="../flash-card-project-start/images/wrong.png")
+wrong_but = Button(width=100, height=100,image=image_wrong, highlightthickness=0, bd=0, bg=BACKGROUND_COLOR)
 wrong_but.grid(column=1,row=2)
 
 window.mainloop()
