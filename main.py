1b05e3af445e6ad63f6fe0752ea54b1c8f6077a7
yacine <jakmca18@gmail.com>
2024-04-27 17:16:20 +0200

    Programme Finished

--- main.py
+++ main.py
@@ -11,36 +11,37 @@ window.minsize(width=1000, height=600)
 window.title("Flashy")
 window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
 file_path = "data/word_to_learn.csv"
+to_learn = {}
+current_card = {}
+# print(type(word_list_to_learn))
 try:
-    word_list = pd.read_csv("../flash-card-project-start/data/french_words.csv")
-    word_list_to_learn = pd.read_csv("../flash-card-project-start/data/word_to_learn.csv")
-except FileNotFoundError:
-    word_list_to_learn = word_list.to_csv(file_path,index=False)
-else:
-    Tran_word_list = word_list.to_dict(orient="records")
-    word_dict_to_learn = word_list_to_learn.to_dict(orient="records")
-    current_card = {}
+    data = pd.read_csv("../flash-card-project-start/data/word_to_learn.csv")
 
+except FileNotFoundError:
+    original_data = pd.read_csv("../flash-card-project-start/data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 # TODO 2 : create function That changes the words
 def next_word():
-    global card_flip_timer, current_card, word_list_to_learn
+    global card_flip_timer, current_card, word_dict_to_learn, to_learn
     window.after_cancel(card_flip_timer)
-    print(word_dict_to_learn)
-    current_card = random.choice(word_dict_to_learn)
-    # index =len(current_card)
-    # print(index)
+    current_card = random.choice(to_learn)
     card.itemconfig(card_title, text=f"French",fill="black" )
     card.itemconfig(card_word, text=current_card['French'], fill="black")
     card.itemconfig(card_background, image=image_card_front)
     card_flip_timer = window.after(3000, func=flip_card)
-    word_dict_to_learn.remove(current_card)
-    print(word_dict_to_learn)
-    # new_data = {'French':word_dict_to_learn[0:]["French"]}
-    # word_list_to_learn.drop()
-    # word_list_to_learn = word_dict_to_learn.(file_path,index=False)
 
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pd.DataFrame(to_learn)
+    print(len(data))
+    data.to_csv("../flash-card-project-start/data/word_to_learn.csv", index=False)
+    next_word()
 
 #TODO 3 : create the fliping function
 def flip_card():
@@ -60,7 +61,7 @@ card_word = card.create_text(400, 263, text="", font=(FONT, 60, "bold"))
 card.grid(column=1, row=1, columnspan=2)
 
 image_right_but = PhotoImage(file="../flash-card-project-start/images/right.png")
-right_but = Button(width=100,height=100,image=image_right_but, bd=0,bg=BACKGROUND_COLOR, highlightthickness=0, command=next_word )
+right_but = Button(width=100,height=100,image=image_right_but, bd=0,bg=BACKGROUND_COLOR, highlightthickness=0, command=is_known )
 right_but.grid(column=2,row=2)
 
 image_wrong = PhotoImage(file="../flash-card-project-start/images/wrong.png")
